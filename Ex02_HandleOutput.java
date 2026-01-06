d3a46155db8ebe7798f5a4237511ec3970bada65
psavita02 <priyanka.savita@telusinternational.com>
2026-01-06 20:50:58 +0530

    Added file2

--- Ex02_HandleOutput.java
+++ Ex02_HandleOutput.java
@@ -5,9 +5,9 @@ public class Ex01_HandleOutput
 {
 public static void main(String[] args) {
 System.out.println(1);
-System.out.println(1.5);
+System.out.println(1.6);
 System.out.println('A');
-System.out.println("Hi World");
+System.out.println("Hiee World");
 System.out.println(true);
 System.out.println("******************************");
 }
