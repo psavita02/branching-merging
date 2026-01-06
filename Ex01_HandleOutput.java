37cea46b3ec7361ce62d8ab4a1ae7be02fe7f636
psavita02 <priyanka.savita@telusinternational.com>
2026-01-06 20:36:35 +0530

    Update Ex01_HandleOutput.java

--- Ex01_HandleOutput.java
+++ Ex01_HandleOutput.java
@@ -10,10 +10,6 @@ System.out.println('A');
 System.out.println("Hello World");
 System.out.println(true);
 System.out.println("******************************");
-System.out.print(1);
-System.out.print(1.5);
-System.out.print('A');
-System.out.print("Hello World");
-System.out.print(true);
 }
 }
+
